1852e3aecadf2edf68607e30cfdd582cba10b6bb
Chris Maffei <61283901+cmaffei720@users.noreply.github.com>
2020-08-08 10:14:39 -0400

    august 8 push to start class (#4)

--- musicFinder/controllers/musicController.js
+++ musicFinder/controllers/musicController.js
@@ -1,61 +1,114 @@
-// var express = require("express");
+// Imports
+var express = require('express');
+var router = express.Router();
+var request = require('request');
 
-// var router = express.Router();
+var db = require('../models');
 
-// // Import the model (cat.js) to use its database functions.
-// var db = require("../models")
+// GET route which calls uses Sequelize's findAll method.
+router.get('/', function(req, res) {
+    db.Artist.findAll({
+        order: 'artist_name ASC'
 
-// // Create all our routes and set up logic within those routes where required.
-// router.get("/", function(req, res) {
-//   db.all(function(data) {
-//     var hbsObject = {
-//       db: data
-//     };
-//     console.log(hbsObject);
-//     res.render("index", hbsObject);
-//   });
-// });
+    }).then(function(data) {
+        var hbsObject = {
+            artists: data
+        };
+        res.render('index', hbsObject);
+    });
+});
 
-// router.post("/api/song", function(req, res) {
-//   db.create([
-//     "name", "added"
-//   ], [
-//     req.body.name, req.body.added
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
+router.get('/song', function(req, res) {
+    db.Song.findAll({
+        order: 'song_name DESC'
 
-// router.put("/api/playlist/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+    }).then(function(data) {
+        var hbsObject = {
+            songs: data
+        };
+        res.render('index', hbsObject);
+    });
+});
 
-//   console.log("condition", condition);
+router.get('/album', function(req, res) {
+    db.Album.findAll({
+        order: 'album_name DESC'
 
-//   db.update({
-//     added: req.body.added
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+    }).then(function(data) {
+        var hbsObject = {
+            albums: data
+        };
+        res.render('index', hbsObject);
+    });
+});
 
-// router.delete("/api/playlist/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
 
-//   db.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+router.post('/api/new/artist', function(req, res) {
 
-// // Export routes for server.js to use.
-// module.exports = router;
+    var artistName = req.body.name;
+
+    var queryUrl = "https://deezerdevs-deezer.p.rapidapi.com/search&q=" + artistName;
+
+    request(queryUrl, function(error, response, body) {
+
+
+        if (!error && JSON.parse(body).Response !== 'False') {
+            console.log(JSON.parse(body));
+
+            var name = JSON.parse(body).artist.name;
+
+            console.log(name);
+
+            var artist = "";
+
+            var options = {
+                method: 'GET',
+                url: 'https://deezerdevs-deezer.p.rapidapi.com/search' + name,
+                qs: {
+                    language: 'en-US',
+                    api_key: '778af56556msh60df0212f929a25p1bd6fajsn95b39208364f'
+                },
+                body: '{}'
+            };
+
+            request(options, function(error, response, result) {
+
+                if (error) res.redirect('/');
+                if (!JSON.parse(result).results) {
+    
+                    res.redirect('/')
+                } else {
+                    artist = JSON.parse(result).results[0].key;
+                    console.log(artist);
+                    db.Artist.create({
+                        artist_name: JSON.parse(body).artist.name,
+                    }).then(function() {
+                        res.redirect('/');
+                    });
+
+                }
+            });
+
+        } else {
+            console.log("\nOops...something went wrong with you musical search. Try again...");
+            res.redirect('/');
+        }
+    });
+});
+
+/////////////////////////////////////////////////////////////////////
+router.put('/api/new/artist/:id', function(req, res) {
+
+    var added = true;
+    var ID = req.params.id;
+
+    db.Artist.update({
+        added: added,
+
+    }, { where: { id: ID } }).then(function() {
+        res.redirect('/');
+    });
+});
+
+
+module.exports = router;
