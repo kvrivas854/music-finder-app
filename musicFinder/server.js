0c04cf7860904bcc098868e25ad6e1c6fc23cfef
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-08-08 11:00:11 -0400

    added in sequelize and removed controller.js from server.js. Replaced with musicRoutes.js

--- musicFinder/server.js
+++ musicFinder/server.js
@@ -24,12 +24,20 @@ app.set("view engine", "handlebars");
 var db = require("./models")
 
 // Import routes and give the server access to them.
-// var routes = require("./controllers/musicController.js");
+var routes = require("./routes/musicRoutes.js");
 
-// app.use(routes);
+app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+
+
+// // Start our server so that it can begin listening to client requests.
+// app.listen(PORT, function() {
+//   // Log (server-side) when our server has started
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
