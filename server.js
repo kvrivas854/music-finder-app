16db4c882bb876acc5fcc5154d2b55b5b534dbe4
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-18 15:14:38 -0400

    navbar hover, took out carousel, adjusted border radius for testimonial cards

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5000;
 
 var app = express();
 
